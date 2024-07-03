71cb33b00a93246bdbdeb439a9f845b9a5f113c9
SAURABHKUMARCHAUHAN1203 <SAURABHKUMARCHAUHAN.SRK@GMAIL.COM>
2024-07-03 18:46:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,16 +18,6 @@ class Config(object):
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     SESSIONS = "./SESSIONS"
 
-    AUTH_USERS = os.environ.get('AUTH_USERS', '7156412102').split(',')
-    for i in range(len(AUTH_USERS)):
-        AUTH_USERS[i] = int(AUTH_USERS[i])
-
-    GROUPS = os.environ.get('GROUPS', '-1001928293542').split(',')
-    for i in range(len(GROUPS)):
-        GROUPS[i] = int(GROUPS[i])
-
-    LOG_CH = os.environ.get("LOG_CH", "-1002040684965")
-
 # TelegramLogHandler is a custom handler which is inherited from an existing handler. ie, StreamHandler.
 logging.basicConfig(
     level=logging.INFO,
